4a0f250c7d0c8a7a4d0dde167c1bbb6cb1c979dc
Ryan Spears <ryan@spears.ca>
2019-03-20 00:29:56 -0400

    mysql wired in

---
+++ util/database.js
@@ -0,0 +1,10 @@
+const mysql = require('mysql2');
+
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    database: 'node-complete',
+    password: '12345six'
+});
+
+module.exports = pool.promise();
